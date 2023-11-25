089008a50d43922f6e142ee4c70dbb9c06556c66
heathernfran <heathernfran@gmail.com>
2023-11-24 16:44:09 -0800

    Fade in each letter of favourite books

--- src/components/Books/Books.tsx
+++ src/components/Books/Books.tsx
@@ -61,7 +61,19 @@ export function Books() {
         <h2>Favourites</h2>
         <ul>
           {favourites.map((favourite) => (
-            <li key={favourite.key}>{favourite.title}</li>
+            <li key={favourite.key}>
+              {favourite.title.split('').map((char, charIndex) => (
+                <span
+                  key={`${favourite.title}_${charIndex}`}
+                  style={{
+                    opacity: 0,
+                    animation: `fadeIn 0.5s forwards ${charIndex * 0.5}s`,
+                  }}
+                >
+                  {char}
+                </span>
+              ))}
+            </li>
           ))}
         </ul>
       </>
